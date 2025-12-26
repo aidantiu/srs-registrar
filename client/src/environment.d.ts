371666768ac3c7a66ff975fd13d999fe2f5de96a
ymnw.r <tiuprof77@gmail.com>
2025-12-26 14:54:13 +0800

    fix: update prod api url

--- client/src/environment.d.ts
+++ client/src/environment.d.ts
@@ -5,7 +5,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_APP_TITLE: string;
+	readonly VITE_APP_ENVIRONMENT: string;
+	readonly VITE_API_URL: string;
+	readonly VITE_APP_TITLE?: string;
 	// more env variables...
 }
 
